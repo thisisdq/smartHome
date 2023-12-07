db2464fc62d55e461313cb6e95f94cb8f949f7f7
DanhQuy <danhquy1101@gmail.com>
2023-12-07 23:51:43 +0700

    setup lại front-end

--- Client/smartHouse/src/app/components/test/test.component.ts
+++ Client/smartHouse/src/app/components/test/test.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component , OnInit,OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -9,23 +9,54 @@ import { Component , OnInit,OnDestroy } from '@angular/core';
   styleUrl: './test.component.scss'
 })
 export class TestComponent implements OnInit, OnDestroy {
-  constructor(){
+  constructor() {
 
   }
 
   public time = Date.now();
-  public timeInterval :any;
+  public timeInterval: any;
 
   ngOnInit(): void {
     this.time = Date.now();
-    this.timeInterval = setInterval(() =>{
+    this.timeInterval = setInterval(() => {
       this.time = Date.now();
-    },1000)
+    }, 1000)
+    this.slide();
   }
 
   ngOnDestroy(): void {
-      if(this.timeInterval){
-        clearInterval(this.timeInterval);
-      }
+    if (this.timeInterval) {
+      clearInterval(this.timeInterval);
+    }
+  }
+
+  slide() {
+    const slider :any = document.querySelector('.items');
+    let isDown = false;
+    let startX:any;
+    let scrollLeft:any;
+
+    slider.addEventListener('mousedown', (e:any) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e:any) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+    });
   }
 }

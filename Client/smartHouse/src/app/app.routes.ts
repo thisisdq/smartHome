ae390b3b45fbe0757bd5fcdc49e9b7d4ec5b2eb1
DanhQuy <110118405+thisisdq@users.noreply.github.com>
2023-12-08 06:53:32 +0700

    net

--- Client/smartHouse/src/app/app.routes.ts
+++ Client/smartHouse/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { SmartHouseControllPageComponent } from './components/smart-house-controll-page/smart-house-controll-page.component';
 import { TestComponent } from './components/test/test.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SmartHosueHomePageComponent } from './smart-hosue-home-page/smart-hosue-home-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginPageComponent, title: 'Login SmartHome' },
     { path: 'smarthousepage', component: SmartHouseControllPageComponent, title: 'USER PAGE' },
+    { path: 'net', component: SmartHosueHomePageComponent, title: 'Ngồi net code chơi'},
     { path: 'test', component: TestComponent, title: 'test' },
     { path: '**', component: PageNotFoundComponent, title: 'Pagenotfound' }];
--- Client/smartHouse/src/app/components/smart-house-controll-page/smart-house-controll-page.component.ts
+++ Client/smartHouse/src/app/components/smart-house-controll-page/smart-house-controll-page.component.ts
@@ -1,77 +1,79 @@
-import { Component } from '@angular/core';
-import { AuthenticationService } from '../../authentication.service';
-import { Router } from '@angular/router';
-import { DEVICE_TYPE, UserAccount } from '../../module';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { AuthenticationService } from "../../authentication.service";
+import { Router } from "@angular/router";
+import { DEVICE_TYPE, UserAccount } from "../../module";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-smart-house-controll-page',
+  selector: "app-smart-house-controll-page",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './smart-house-controll-page.component.html',
-  styleUrl: './smart-house-controll-page.component.scss'
+  templateUrl: "./smart-house-controll-page.component.html",
+  styleUrl: "./smart-house-controll-page.component.scss",
 })
 export class SmartHouseControllPageComponent {
   constructor(
     private authService: AuthenticationService,
-    private router: Router,
-  ) {
-
-  }
+    private router: Router
+  ) {}
 
   public time = Date.now();
-  public timeInterval :any;
+  public timeInterval: any;
 
   ngOnInit() {
     this.time = Date.now();
-    this.timeInterval = setInterval(() =>{
+    this.timeInterval = setInterval(() => {
       this.time = Date.now();
-    },1000)
+    }, 1000);
   }
 
   ngOnDestroy() {
-    if(this.timeInterval){
+    if (this.timeInterval) {
       clearInterval(this.timeInterval);
     }
   }
 
   public acc: UserAccount = {
-    fullname: 'DanhQuy',
-    username: 'admin',
-    password: 'password',
-    house: [{
-      houseID: 1,
-      address: '1 Dai Co Viet',
-      floors: [
-        {
-          floorID: 1,
-          floorName: 'Tầng 1',
-          rooms: [{
-            roomID: 1,
-            roomName: 'Living Room',
-            devices: [
+    fullname: "DanhQuy",
+    username: "admin",
+    password: "password",
+    house: [
+      {
+        houseID: 1,
+        address: "1 Dai Co Viet",
+        floors: [
+          {
+            floorID: 1,
+            floorName: "Tầng 1",
+            rooms: [
               {
-                deviceID: 1,
-                deviceName: 'TV',
-                deviceType: DEVICE_TYPE.TV,
-                deviceStatus: 'OFF',
-                deviceValue: undefined,
-                isRuning: false,
+                roomID: 1,
+                roomName: "Living Room",
+                devices: [
+                  {
+                    deviceID: 1,
+                    deviceName: "TV",
+                    deviceType: DEVICE_TYPE.TV,
+                    deviceStatus: "OFF",
+                    deviceValue: undefined,
+                    isRuning: false,
+                  },
+                  {
+                    deviceID: 2,
+                    deviceName: "Light Tile",
+                    deviceType: DEVICE_TYPE.LED,
+                    deviceStatus: "ON",
+                    deviceValue: undefined,
+                    isRuning: true,
+                  },
+                ],
               },
-              {
-                deviceID: 2,
-                deviceName: 'Light Tile',
-                deviceType: DEVICE_TYPE.LED,
-                deviceStatus: 'ON',
-                deviceValue: undefined,
-                isRuning: true
-              },
-            ]
-          }]
-        }
-      ]
-    }]
-  }
+            ],
+          },
+        ],
+      },
+    ],
+  };
 
   test() {
     console.log(new Date());
@@ -79,6 +81,6 @@ export class SmartHouseControllPageComponent {
 
   logout() {
     this.authService.logout();
-    this.router.navigateByUrl('/')
+    this.router.navigateByUrl("/");
   }
 }
